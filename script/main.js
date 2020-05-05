893bd5a7cb1b67faaceef61b6eb5bb69fb44e91a
Loïc Cattani <loic@cattani.co>
2020-05-05 19:04:03 +0200

    Refactorisation

--- script/main.js
+++ script/main.js
@@ -1,42 +1,60 @@
+
+const countries = {
+    'Argentina': [327, 433],
+}
+
 const svg = d3
 .select('body')
 .append('svg')
 .attr('width', 2000)
 .attr('height', 1000);
 
+// Création des groupes
+const mapGroup = svg.append('g');
+const burgerGroup = svg.append('g');
 
-// Ajout de l'élément burger svg
-const bur = svg.append('g')
-.append('text')
-.attr('x', 20)
-.attr('y', 20)
-.text('🍔')
-.attr('transform', 'scale(20, 20)');
-// pour changer la grandeur de l'image du burger : 
-//.attr('transform', 'scale(10, 10)');
+const projection = d3.geoNaturalEarth1().scale(200);
+const pathGenerator = d3.geoPath().projection(projection);
+
+d3.json('./map.json').then(onDataLoaded);
+
+// Dev pour trouver les positions
+svg.on('click', function() {
+    const coords = d3.mouse(this);
+    console.log(
+        Math.round(coords[0]),
+        Math.round(coords[1])
+    );
+})
 
 //Créer l'animation
 
+function onDataLoaded(data) {
+    drawMap(data);
+}
 
-var burger = document.querySelector('#burger');
-
-burger.style.left = 300 + "px";
-burger.style.top = 200 + "px";
-setTimeout(function() {
-    burger.style.transform = 'scale(10)';
-}, 200);
-
-
-const projection = d3.geoNaturalEarth1().scale(250);
-const pathGenerator = d3.geoPath().projection(projection);
-
-d3.json('./map.json').then(function (data) {
+function drawMap(data) {
     const features = data.features;
-    console.log(features)
-    const paths = svg
+    //console.log(features)
+    mapGroup
     .selectAll('path')
     .data(features)
     .enter()
     .append('path')
     .attr('d', (d) => pathGenerator(d));
-});
+}
+
+function drawBurger(pos) {
+    // Ajout de l'élément burger svg
+    burgerGroup
+    .append('text')
+    .attr('x', pos[0] - 15)
+    .attr('y', pos[1] + 15)
+    .text('🍔')
+    .style('font-size', 30)
+    .attr('transform', 'scale(1)');
+    // pour changer la grandeur de l'image du burger : 
+    //.attr('transform', 'scale(10, 10)');
+}
+
+drawBurger(countries['Argentina']);
