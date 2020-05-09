46607db16b2178d8f864ea8df82938c42dea0308
ssaeli <63305108+ssaeli@users.noreply.github.com>
2020-05-09 10:47:57 +0200

    update main.js

--- script/main.js
+++ script/main.js
@@ -51,67 +51,80 @@ const svg = d3
 .attr('height', 610);
 
 
-d3.dsv(';', 'data/dati.csv', function(d){
-    return {
-        Date : d.Date.substr(6,11),
-        Country : d.Country,
-        size : parseFloat(d.size.substr(0,10)),
-        
-    }
-}).then(function(data){
-    console.log(data);
+var years = {
+    2000: [
+        { country: "Argentina", size: "0.30832404" },
+        { country: "Australia", size: "1.59337026" },
+        { country: "Brazil", size: "0.22752740" },
+        { country: "Britain", size: "0.93770386" },
+        { country: "Canada", size: "1.24770761" },
+        { country: "Chile", size: "0.20702376" },
+        { country: "China", size: "0.08023842" },
+        { country: "Czech Republic", size: "0.43232325" },
+        { country: "Denmark", size: "0.99869948 " },
+        { country: "Euro area", size: "0.84705003" },
+        { country: "Hong Kong", size: "1.96710206" },
+        { country: "Hungary", size: "0.38058247" },
+        { country: "Indonesia", size: "0.04274904" },
+        { country: "Israel", size: "0.58776881" },
+        { country: "Japan", size: "1.38925045" },
+        { country: "Malaysia", size: "0.33995388" },
+        { country: "Mexico", size: "0.32227289" },
+        { country: "New Zealand", size: "0.80642989" },
+        { country: "Poland", size: "0.35124961" },
+        { country: "Russia", size: "0.12782414" },
+        { country: "Singapore", size: "1.26715487" },
+        { country: "South Africa", size: "0.22642122" },
+        { country: "South Korea", size: "0.44126392" },
+        { country: "Sweden", size: "1.08496519" },
+        { country: "Switzerland", size: "1.09112039" },
+        { country: "Thailand", size: "0.13871633" },
+        { country: "United States", size: "1.44760592" },
+    ],
+};
 
-var year2000 = [
-{'date': '2000', 'country': "Argentina", 'size': '0.30832404' },
-{ 'date': "2000", 'country': "Australia", 'size': '1.59337026'},
-{ 'date': "2000", 'country': "Brazil", 'size': '0.22752740' },
-{ 'date': "2000", 'country': "Britain", 'size': '0.93770386' },
-{ 'date': "2000", 'country': "Canada", 'size': '1.24770761' },
-{ 'date': "2000", 'country': "Chile", 'size': '0.20702376' },
-{ 'date': "2000", 'country': "China", 'size': '0.08023842' },
-{ 'date': "2000", 'country': "Czech Republic", 'size': '0.43232325' },
-{ 'date': "2000", 'country': "Denmark", 'size': '0.99869948 '},
-{ 'date': "2000", 'country': "Euro area", 'size': '0.84705003' },
-{ 'date': "2000", 'country': "Hong Kong", 'size': '1.96710206' },
-{ 'date': "2000", 'country': "Hungary", 'size': '0.38058247' },
-{ 'date': "2000", 'country': "Indonesia", 'size': '0.04274904' },
-{ 'date': "2000", 'country': "Israel", 'size': '0.58776881' },
-{ 'date': "2000", 'country': "Japan", 'size': '1.38925045'},
-{ 'date': "2000", 'country': "Malaysia", 'size': '0.33995388' },
-{ 'date': "2000", 'country': "Mexico", 'size': '0.32227289' },
-{ 'date': "2000", 'country': "New Zealand", 'size': '0.80642989' },
-{ 'date': "2000", 'country': "Poland", 'size': '0.35124961' },
-{ 'date': "2000", 'country': "Russia", 'size': '0.12782414'},
-{ 'date': "2000", 'country': "Singapore", 'size': '1.26715487' },
-{ 'date': "2000", 'country': "South Africa", 'size': '0.22642122' },
-{ 'date': "2000", 'country': "South Korea", 'size': '0.44126392' },
-{ 'date': "2000", 'country': "Sweden", 'size': '1.08496519' },
-{ 'date': "2000", 'country': "Switzerland", 'size': '1.09112039' },
-{ 'date': "2000", 'country': "Thailand", 'size': '0.13871633' },
-{ 'date': "2000", 'country': "United States", 'size': '1.44760592' }
-]
+    d3.dsv(";", "data/dati.csv", function (d) {
+        return {
+            date: d.Date.substr(6, 11),
+            country: d.Country,
+            size: parseFloat(d.size.substr(0, 10)),
+        };
+    }).then(function (data) {
+      //-----------------
+        // organizing data
+      //-----------------
+      
+      let years = {};
+        data.forEach((d) => {
+            let country = d.country;
+            if (!(country in years)) {
+                years[country] = [];
+            }
+            years[country].push({country: d.country, size: d.size});
+        });
 
+const size = data.size
 
+console.log(data)
+console.log(years)
 
 
 const mapGroup = svg.append('g')
 const burgerGroup = svg.append('g')
 
-
 function drawBurger(pos) {
-
-
     burgerGroup
     .append('text')
     .attr('x', pos[0] - 15)
     .attr('y', pos[1] + 15)
     .text('🍔');
-}
+};
 
 const projection = d3.geoNaturalEarth1().scale(250).translate([625, 360]);
 const pathGenerator = d3.geoPath().projection(projection);
 
 d3.json('./map.json').then(onDataLoaded);
+
 
 
 svg.on('click', function() {
